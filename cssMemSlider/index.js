10594e8b8e8f11aa166b5d2bedf789c0a69a9e3c
Louise-Birulia-Bezenson <elizabethbirulia@gmail.com>
2022-05-16 21:04:39 +0300

    feat: add animation

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,13 +1,14 @@
 const slider = document.querySelectorAll('.slider'),
-      dots = document.querySelectorAll('.dot');
-
-let index = 0;
+      dots = document.querySelectorAll('.dot'),
+      bDots = document.querySelectorAll('.big-dot'),
+      content = document.querySelector('.slider-content');
 
 const activeSlide = n => {
-    for (let slide of slider){
-        slide.classList.remove('active');
-    }
-    slider[n].classList.add('active');
+    content.style.transform = `translateY(-${100/4 * n}%)`;
+    // for (let slide of slider){
+    //     slide.classList.remove('active');
+    // }
+    // slider[n].classList.add('active');
 }
 
 const activeDot = n => {
@@ -16,10 +17,21 @@ const activeDot = n => {
     }
     dots[n].classList.add('active');
 } 
+const activebDot = n => {
+    for (let bdot of bDots){
+        bdot.classList.remove('active');
+    }
+    bDots[n].classList.add('active');
+} 
 
-dots.forEach((item,indexDot) =>{
+dots.forEach((item, index) =>{
     item.addEventListener('click', () =>{
-    index = indexDot;
-    activeSlide (index);
-    })
-})
+        activeSlide(index);
+    });
+});
+
+bDots.forEach((item, index) =>{
+    item.addEventListener('click', () =>{
+        activeSlide(index);
+    });
+});
